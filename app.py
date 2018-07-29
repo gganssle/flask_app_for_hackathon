2ce48b3697fb22e497a7318b7628d14db929c0b8
GRAM <gr@m-gan.sl>
2018-07-29 09:46:16 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, jsonify
+import numpy as np
+import matplotlib.pyplot as plt
+import requests
+
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/')
+def jinja_template():
+    points = np.random.randn(10)
+    plt.plot(points)
+    plt.savefig("static/myfig.png")
+
+    dictionary = {'team': 'Expero', 'project': 'machine learning'}
+    image_location = 'static/myfig.png'
+    return render_template('jinja_template.html', logo=image_location, content=dictionary)
+
+@app.route('/API', methods = ['GET'])
+def api():
+    if request.method == 'GET':
+        dictionary = {'var1':request.args.get('arg1'),
+                      'var2':request.args.get('arg2')}
+    return jsonify(dictionary)
+# to access the above, use:
+# http://127.0.0.1:5000/API?arg1=One&arg2=Two
+
+
+
+if __name__ == '__main__':
+    app.run()
